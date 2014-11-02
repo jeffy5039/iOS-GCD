c3274f4aa8452ec6af9d65ef4f70def0f8abd872
Jordan Byrne <jordan5039@live.com>
2014-11-02 12:25:47 +0000

    first commit

--- Grand Central Dispatch/ViewController.swift
+++ Grand Central Dispatch/ViewController.swift
@@ -12,14 +12,24 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //move to the main thread
+        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+            //display an alert
+            let alert = UIAlertController(title: "GCD", message: "running on main thread", preferredStyle: .Alert)
+            let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+            alert.addAction(action)
+            self.presentViewController(alert, animated: true, completion: nil)
+            
+            //print both the main and current thread, you will see that they are the same.
+            println("main thread = \(NSThread.mainThread())")
+            println("current thread = \(NSThread.currentThread())")
+        })
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
 
 }
 
